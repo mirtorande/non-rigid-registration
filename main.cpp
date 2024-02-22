51d5774b9eb4ee527e974991f58c82b61fae56b3
mirtorande <mirtorande@gmail.com>
2024-02-22 16:11:28 +0100

    Multiresolution graph groundwork

--- main.cpp
+++ main.cpp
@@ -14,6 +14,18 @@ using namespace Eigen;
 
 const std::string MODEL_FILE_PATH = "../resources/";
 
+struct Vertex {
+	int id; // Identificatore del vertice
+	double area; // Area duale del vertice
+	RowVector3d color; // Colore associato all'area del vertice
+
+	// Costruttore per inizializzare un vertice con l'id specificato e un'area predefinita
+	Vertex(int _id) : id(_id), area(1.0) {
+		// Genera un colore casuale per l'area del vertice
+		color << ((double)rand() / RAND_MAX), ((double)rand() / RAND_MAX), ((double)rand() / RAND_MAX);
+	}
+};
+
 void select_n_random_points(int n, const MatrixXd& V, MatrixXd& VA);
 
 void initialize_icp_correspondences(
@@ -29,21 +41,63 @@ void rigid_shape_matching(
 
 void ransac3(const MatrixXd& VX, const MatrixXd& VY, const MatrixXi& FY, const igl::AABB<MatrixXd, 3> Ytree, Matrix3d& R, RowVector3d& t);
 
+void create_multiresolution_hierarchy(MatrixXd& vertices, MatrixXi& faces, MatrixXd& normals, MatrixXd& colors);
+
+// Funzione per trovare i vicini di ciascun vertice nella mesh
+MatrixXi find_vertex_neighbors(const MatrixXi& FB, int num_vertices) {
+	// Creazione della matrice dei vicini dei vertici
+	MatrixXi vertex_neighbors = MatrixXi::Zero(num_vertices, 10); // Si suppone che ogni vertice abbia al massimo 10 vicini
+
+	// Contatore per tener traccia del numero di vicini di ciascun vertice
+	VectorXi num_neighbors = VectorXi::Zero(num_vertices);
+
+	// Per ogni faccia nella matrice delle facce
+	for (int i = 0; i < FB.rows(); ++i) {
+		int v0 = FB(i, 0);
+		int v1 = FB(i, 1);
+		int v2 = FB(i, 2);
+
+		// Aggiungi gli archi tra i vertici della faccia
+		if (num_neighbors[v0] < vertex_neighbors.cols()) {
+			vertex_neighbors(v0, num_neighbors[v0]++) = v1;
+			vertex_neighbors(v0, num_neighbors[v0]++) = v2;
+		}
+		if (num_neighbors[v1] < vertex_neighbors.cols()) {
+			vertex_neighbors(v1, num_neighbors[v1]++) = v0;
+			vertex_neighbors(v1, num_neighbors[v1]++) = v2;
+		}
+		if (num_neighbors[v2] < vertex_neighbors.cols()) {
+			vertex_neighbors(v2, num_neighbors[v2]++) = v0;
+			vertex_neighbors(v2, num_neighbors[v2]++) = v1;
+		}
+	}
+
+	return vertex_neighbors;
+}
+
 
 int main(int argc, char* argv[])
 {
 	MatrixXd OVX, VA, VB;
 	MatrixXi FA, FB;
 	igl::readOBJ(MODEL_FILE_PATH + "rabbit.obj", VA, FA);
-	igl::readOBJ(MODEL_FILE_PATH + "rabbit-shard2.obj", VB, FB);
+	igl::readOBJ(MODEL_FILE_PATH + "rabbit.obj", VB, FB);
 
 	// Initialize viewer
 	igl::opengl::glfw::Viewer v;
 
 	igl::AABB<MatrixXd, 3> treeB;
 	treeB.init(VB, FB);
-	MatrixXd NY;
-	igl::per_face_normals(VB, FB, NY);
+	MatrixXd NB;
+	igl::per_face_normals(VB, FB, NB);
+
+	Eigen::MatrixXd colors(VB.rows(), 3);
+	for (int i = 0; i < colors.rows(); ++i) {
+		colors.row(i) = Eigen::RowVector3d::Random().cwiseAbs();
+	}
+
+	// Create multiresolution hierarchy
+	create_multiresolution_hierarchy(VB, FB, NB, colors);
 
 	const auto apply_random_rotation = [&]()
 		{
@@ -89,12 +143,6 @@ int main(int argc, char* argv[])
 		{
 			switch (key)
 			{
-			case ' ':
-			{
-				v.core().is_animating = false;
-				single_iteration();
-				return true;
-			}
 			case 'R':
 			case 'r':
 			{
@@ -152,11 +200,12 @@ int main(int argc, char* argv[])
 		};
 
 	v.data().set_mesh(VB, FB);
-	v.data().set_colors(RowVector3d(1, 0, 1));
+	v.data().set_colors(colors);
+	//v.data().set_colors(RowVector3d(1, 0, 1));
 	v.data().show_lines = false;
-	v.append_mesh();
-	v.data().set_mesh(VA, FA);
-	v.data().show_lines = false;
+	//v.append_mesh();
+	//v.data().set_mesh(VA, FA);
+	//v.data().show_lines = false;
 	v.launch();
 }
 
@@ -298,4 +347,81 @@ void ransac3(const MatrixXd& VX, const MatrixXd& VY, const MatrixXi& FY, const i
 	}
 	R = best_R;
 	t = best_t;
+}
+
+// Funzione per creare la gerarchia a risoluzione multipla
+void create_multiresolution_hierarchy(MatrixXd& vertices, MatrixXi& faces, MatrixXd& normals, MatrixXd& colors) {
+	// Inizializza il generatore di numeri casuali con il tempo corrente
+	//srand(time(nullptr));
+
+	// Trova i vicini di ciascun vertice
+	MatrixXi vertex_neighbors = find_vertex_neighbors(faces, vertices.rows());
+
+	// Stampa i vicini di ciascun vertice
+	for (int i = 0; i < vertices.rows(); ++i) {
+		std::cout << "Vertice " << i << " ha vicini: ";
+		for (int j = 0; j < vertex_neighbors.row(i).cols(); ++j) {
+			if (vertex_neighbors(i, j) != -1) {
+				std::cout << vertex_neighbors(i, j) << " ";
+			}
+		}
+		std::cout << std::endl;
+	}
+
+	// Variabile booleana per controllare se è stato effettuato almeno un collasso
+	bool collapsed = true;
+
+	// Ripeti finché un punto fisso non è raggiunto (nessun collasso è stato eseguito durante un'iterazione)
+	//while (collapsed) 
+	for (int i = 0; i < 1820; i++) {
+		collapsed = false; // Resetta il flag di collasso
+
+		// Fase (a): calcola i punteggi per tutti i vertici vicini e ordina in ordine decrescente
+		//MatrixXd scores = compute_scores(vertices, normals, vertex_neighbors);
+		
+		// Fase (a): calcola i punteggi per tutti i vertici vicini e ordina in ordine decrescente
+		// Assign a score to each vertex equal to the dot product of the normal of the vertex and the normal of the neighbor
+		MatrixXd scores = MatrixXd::Zero(vertices.rows(), 1);
+		for (int i = 0; i < vertices.rows(); ++i) {
+			for (int j = 0; j < vertex_neighbors.row(i).cols(); ++j) {
+				if (vertex_neighbors(i, j) != -1) {
+					// Calcola il punteggio come prodotto scalare tra la normale del vertice e la normale del vicino
+					scores(i) += igl::dot(normals.row(i).data(), normals.row(vertex_neighbors(i, j)).data());
+				}
+			}
+		}
+
+		// Fase (b): esamina i punteggi e collassa i vertici se necessario
+		for (int i = 0; i < vertices.rows(); ++i) {
+			// Trova il vertice con il punteggio più basso tra i vicini
+			int min_neighbor = -1;
+			double min_score = std::numeric_limits<double>::max();
+			for (int j = 0; j < vertex_neighbors.row(i).cols(); ++j) {
+				if (vertex_neighbors(i, j) != -1 && scores(vertex_neighbors(i, j)) < min_score) {
+					min_neighbor = vertex_neighbors(i, j);
+					min_score = scores(vertex_neighbors(i, j));
+				}
+			}
+
+			// Se il punteggio del vertice corrente è inferiore a quello del vicino, collassa il vertice corrente
+			if (scores(i) < min_score) {
+				// Collassa il vertice corrente nel vicino con il punteggio più basso
+				vertices.row(min_neighbor) = (vertices.row(min_neighbor) + vertices.row(i)) / 2.0;
+				colors.row(min_neighbor) = (colors.row(min_neighbor) + colors.row(i)) / 2.0;
+
+				//assegna ai vertici collassati lo stesso colore casuale
+				colors.row(i) = colors.row(min_neighbor);
+
+				// Imposta il flag di collasso
+				collapsed = true;
+			}
+		}
+	}
+
+	// Assegna i colori casuali alle aree delle mesh associate ai vertici nella gerarchia
+	colors.resize(vertices.rows(), 3);
+	for (int i = 0; i < vertices.rows(); ++i) {
+		// Genera un colore casuale per ogni area della mesh
+		colors.row(i) << ((double)rand() / RAND_MAX), ((double)rand() / RAND_MAX), ((double)rand() / RAND_MAX);
+	}
 }
